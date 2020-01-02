c4b8ba47f3f36efc0e2123d38b2adacbcb986447
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-01-02 00:39:31 -0600

    Redux on playground folder

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -5,11 +5,53 @@ export default function redux() {
 
 //REDUX LESSON
 
-    const store = createStore((state = { count: 0}) => {
-    return state;
+const store = createStore((state = { count: 0}, action) => {
+    switch(action.type){
+        case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+            return {count: state.count + incrementBy}
+            case 'DECREMENT': 
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
+            return {count: state.count - decrementBy}
+        case 'RESET':
+            return {count: 0}
+        default:
+            return state;
+    }
 });
 
-console.log(store.getState());
+store.subscribe(() =>{
+    console.log('Current state', store.getState());
+})
+
+
+//Actions - is an object that get sent to the store, describes the type of action. Changes the state overtime 
+
+//increment action
+store.dispatch({
+    type : 'INCREMENT',
+    incrementBy: 5
+});
+store.dispatch({
+    type : 'INCREMENT'
+});
+
+//reset action
+store.dispatch({
+    type: 'RESET'
+})
+
+//decrement action
+store.dispatch({
+    type: 'DECREMENT',
+})
+
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+})
+
+
 
 //
     return (
